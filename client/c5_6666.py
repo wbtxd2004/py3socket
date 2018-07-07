20a6d7a9e9aa1b23eaebf39cb642e4d0801aae8f
WuBin <wbtxd2004@gmail.com>
2018-07-07 14:53:06 +0800

    0707

--- client/c5_6666.py
+++ client/c5_6666.py
@@ -8,14 +8,11 @@ import time
 
 # 创建 socket 对象
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # 获取本地主机名
 # host = socket.gethostname()
 host = '47.104.170.183'
-
 # 设置端口号
 port = 6666
-
 # 连接服务，指定主机和端口
 client.connect((host, port))
 while True:
@@ -23,3 +20,4 @@ while True:
     # print("the data received is:",data)
     client.send(b"hihi I am client")
     client.close()
+    break
--- server/s5_6666.py
+++ server/s5_6666.py
@@ -8,38 +8,39 @@ import struct
 import os
 import time
 
-# 创建 socket 对象
-serversocket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
-
-# 获取本地主机名
-host = socket.gethostname()
-# host = '47.104.170.183'
-
-port = 6666
-
-# 绑定端口号
-serversocket.bind((host, port))
-
-# 设置最大连接数，超过后排队
-serversocket.listen(5)
-
-print('Server start at: %s:%s' %(host, port))
-print ('wait for connection...')
-
-while True:
-    # 建立客户端连接
-    cs, addr = serversocket.accept()
-    # print("get connected from:", addr)
-    cs.send(b"begin")
-    ra = cs.recv(1024)
-    cs.send(ra)
-    cs.send(b"over")
-    # print(ra)
-    cs.close()
-    now_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
-    file_path = '/opt/socket/'+now_time+'.txt'
-    f = open(file_path , 'wb')
-    f.write(ra)
-    f.close()
-    # print('save in', file_path)
+try:
+    # 创建 socket 对象
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 获取本地主机名
+    host = socket.gethostname()
+    # host = '47.104.170.183'
+    port = 6666
+    # 绑定端口号
+    serversocket.bind((host, port))
+    # 设置最大连接数，超过后排队
+    serversocket.listen(5)
+    print('Server start at: %s:%s' %(host, port))
+    while True:
+        print('wait for connection...')
+        # 建立客户端连接
+        cs, addr = serversocket.accept()
+        # print("get connected from:", addr)
+        print('...connected from:', addr)
+        while True:
+            # cs.send(b"begin")
+            ra = cs.recv(1024)
+            # cs.send(ra)
+            # cs.send(b"over")
+            print(ra)
+            cs.close()
+            if not ra:
+                break
+            else:
+                now_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+                file_path = '/opt/socket/'+now_time+'.txt'
+                f = open(file_path , 'wb')
+                f.write(ra)
+                f.close()
+        # print('save in', file_path)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
